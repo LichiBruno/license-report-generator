f75fd0977a1f2a05f86477cba04dc78c6973d06e
LichiBruno <lichiii@pm.me>
2024-05-28 14:27:57 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# License Checker Script
+
+This script generates license reports for the direct and all dependencies of a Node.js project. It can be run using `npm` or `yarn`, and pre-built executables are also available for Windows, macOS, and Linux.
+
+## Features
+
+- Generates a markdown report for direct dependencies.
+- Generates a markdown report for all dependencies.
+- Supports running via `npm`, `yarn`, or as a standalone executable.
+
+## Prerequisites
+
+- Node.js (if running with `npm` or `yarn`)
+- `npm` or `yarn` installed globally
+
+## Usage
+
+### Running with `npm` or `yarn`
+
+1. **Clone the repository:**
+
+   ```sh
+   git clone https://github.com/LichiBruno/license-report-generator
+   cd license-checker-script
+
+2. **Install dependencies:**
+
+   ```sh
+   npm install
+   # or 
+   yarn install
+   ```
+
+3. **Run the script:**
+
+   ```sh
+   node index.js <path-to-project>
+   ```
+
+   Replace `<path-to-project>` with the path to your project directory.
+   ```
+
+### Running with pre-built executables
+  Pre-built executables are available for Windows, macOS, and Linux. You can download the appropriate executable for your system from the Releases page.
+
+  1. **Download the executable:**
+
+     - Windows: https://github.com/LichiBruno/license-report-generator/releases/download/1.0.0/license-report-generator-win.exe
+     - macOS: https://github.com/LichiBruno/license-report-generator/releases/download/1.0.0/license-report-generator-macos
+     - Linux: https://github.com/LichiBruno/license-report-generator/releases/download/1.0.0/license-report-generator-linux
+
+  2. **Make the executable file runnable (Linux and macOS):**
+
+     ```sh
+     chmod +x license-checker-script-darwin-x64
+     # or
+     chmod +x license-checker-script-linux-x64
+     ```
+  
+  3. **Run the executable:**
+
+     ```sh
+     ./license-checker-script-darwin-x64 <path-to-project>
+     # or
+     ./license-checker-script-linux-x64 <path-to-project>
+     # or for Windows
+     license-checker-script-win32.exe <path-to-project>
+     ```
+
+## Output
+The script will generate two markdown files in the directory where the script is executed:
+ - `DIRECT_DEPENDENCIES_LICENSES_REPORT.md`: This file contains a list of all direct dependencies of the project, along with their licenses and copyright information.
+ - `ALL_DEPENDENCIES_LICENSES_REPORT.md`: This file contains a list of all dependencies of the project, along with their licenses and copyright information.
+
+
+## Contributing
+Contributions are welcome! If you have any suggestions or improvements, please open an issue or submit a pull request.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/LichiBruno/license-report-generator/blob/main/LICENSE) file for details.
---
+++ index.js
@@ -0,0 +1,118 @@
+const { exec } = require("child_process");
+const fs = require("fs").promises;
+const path = require("path");
+
+const projectPath = process.argv[2];
+
+if (!projectPath) {
+  console.error("Please specify the project path as an argument.");
+  process.exit(1);
+}
+
+async function readDirectDependencies(projectPath) {
+  try {
+    const packageJsonPath = path.join(projectPath, "package.json");
+    const packageJson = JSON.parse(await fs.readFile(packageJsonPath, "utf8"));
+    return Object.keys(packageJson.dependencies || {});
+  } catch (error) {
+    console.error("Error reading package.json:", error);
+    process.exit(1);
+  }
+}
+
+function formatReportLine([key, value]) {
+  let packageName = key;
+  let packageVersion = "N/A";
+
+  const versionIndex = key.lastIndexOf("@");
+  if (versionIndex > 0) {
+    packageName = key.substring(0, versionIndex);
+    packageVersion = key.substring(versionIndex + 1);
+  }
+
+  const repositoryUrl = value.repository || "URL not available";
+  return `| ${packageName} | ${packageVersion} | ${value.licenses} | ${repositoryUrl} |`;
+}
+
+async function generateReport(
+  packages,
+  directDependencies,
+  outputPath,
+  reportType
+) {
+  try {
+    const packageEntries = Object.entries(packages);
+    const reportLines = packageEntries
+      .filter(([key]) => {
+        const packageName = key.startsWith("@")
+          ? key.split(/(?<!^)@/)[0]
+          : key.split("@")[0];
+        return reportType === "direct"
+          ? directDependencies.includes(packageName)
+          : true;
+      })
+      .map(formatReportLine);
+
+    const reportContent = `| Name | Version | License | URL |\n| --- | --- | --- | --- |\n${reportLines.join(
+      "\n"
+    )}`;
+    await fs.writeFile(outputPath, reportContent);
+    console.log(
+      `Successfully generated ${reportType} dependencies license file: ${outputPath}`
+    );
+  } catch (error) {
+    console.error(`Error generating ${reportType} report:`, error);
+  }
+}
+
+async function main() {
+  try {
+    const directDependencies = await readDirectDependencies(projectPath);
+
+    exec(
+      `license-checker --start "${projectPath}" --json --production`,
+      async (err, stdout, stderr) => {
+        if (err) {
+          console.error(`Error while executing license-checker: ${err}`);
+          return;
+        }
+        if (stderr) {
+          console.error(`Error: ${stderr}`);
+          return;
+        }
+
+        try {
+          const packages = JSON.parse(stdout);
+          const currentDir = process.cwd();
+          const directOutputPath = path.join(
+            currentDir,
+            "DIRECT_DEPENDENCIES_LICENSES_REPORT.md"
+          );
+          const allOutputPath = path.join(
+            currentDir,
+            "ALL_DEPENDENCIES_LICENSES_REPORT.md"
+          );
+
+          await generateReport(
+            packages,
+            directDependencies,
+            directOutputPath,
+            "direct"
+          );
+          await generateReport(
+            packages,
+            directDependencies,
+            allOutputPath,
+            "all"
+          );
+        } catch (error) {
+          console.error("Error processing license information:", error);
+        }
+      }
+    );
+  } catch (error) {
+    console.error("Error in main function:", error);
+  }
+}
+
+main();
